0640c483484aca1337ce54a1fe4d49551b340327
taka110 <bigtree.aska@gmail.com>
2020-12-11 00:09:10 +0900

    コマンドライン引数・オプションの処理を作成

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 require 'date'
+require 'optparse'
 
 today = Date.today
 yyyy = today.year
 MM = today.month
-
 puts "     #{MM}月  #{yyyy}"
+
+year_month_options = ARGV.getopts("y:m:")  
+puts "     #{year_month_options["m"]}月  #{year_month_options["y"]}"
