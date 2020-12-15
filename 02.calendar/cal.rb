2a224270a3e6670d2d9d88547c83e6a4bea9eb1e
taka110 <bigtree.aska@gmail.com>
2020-12-15 23:39:28 +0900

    月の最終日を取得・カレンダー部分の繰り返し処理

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,17 +4,36 @@ require 'optparse'
 
 year_month_options = ARGV.getopts("y:m:")
 
+#オプションの有無を判定し、無ければ今日の年月を代入する
 if year_month_options["m"] != nil
-  MM = year_month_options["m"]
+  month = year_month_options["m"].to_i
 else
-  MM = Date.today.month
+  month = Date.today.month
 end
 
 if year_month_options["y"] != nil
-  yyyy = year_month_options["y"]
+  year = year_month_options["y"].to_i
 else
-  yyyy = Date.today.year
+  year = Date.today.year
 end
 
-puts "      #{MM}月 #{yyyy}"
-puts "日 月 火 水 木 金 土"
+puts "      #{month}月 #{year}"
+puts " 日 月 火 水 木 金 土"
+
+last_day_of_month = Date.new(year, month ,-1) #月の最終日を取得
+
+(1..last_day_of_month.day).each do |x| #1日から月の最終日まで繰り返し処理
+  if x == 1 #1日がどの曜日か判定し、インデント調整する
+    Date.new(year, month, x).wday.times do
+      print "   "
+    end
+  end
+  if Date.new(year, month, x).wday == 6
+    print " " if x < 10 #1〜9日の時に10の位にスペースを追加
+    puts " #{x}"
+  else
+    print " " if x < 10 #1〜9日の時に10の位にスペースを追加
+    print " #{x}"
+  end
+end
+puts ""
