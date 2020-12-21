4741cae52305f68068e77b0542a02ab1321e7273
taka110 <bigtree.aska@gmail.com>
2020-12-22 00:24:12 +0900

    PRでの指摘を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,17 +5,11 @@ require 'optparse'
 year_month_options = ARGV.getopts("y:m:")
 
 #オプションの有無を判定し、無ければ今日の年月を代入する
-if year_month_options["m"] != nil
-  month = year_month_options["m"].to_i
-else
-  month = Date.today.month
-end
+month = year_month_options["m"].to_i
+month = Date.today.month if month == 0
 
-if year_month_options["y"] != nil
-  year = year_month_options["y"].to_i
-else
-  year = Date.today.year
-end
+year = year_month_options["y"].to_i
+year = Date.today.year if year == 0
 
 puts "      #{month}月 #{year}"
 puts " 日 月 火 水 木 金 土"
@@ -24,16 +18,12 @@ last_day_of_month = Date.new(year, month ,-1) #月の最終日を取得
 
 (1..last_day_of_month.day).each do |x| #1日から月の最終日まで繰り返し処理
   if x == 1 #1日がどの曜日か判定し、インデント調整する
-    Date.new(year, month, x).wday.times do
-      print "   "
-    end
+    print "   " * Date.new(year, month, x).wday
   end
   if Date.new(year, month, x).wday == 6
-    print " " if x < 10 #1〜9日の時に10の位にスペースを追加
-    puts " #{x}"
+    puts sprintf("% 3d", x)
   else
-    print " " if x < 10 #1〜9日の時に10の位にスペースを追加
-    print " #{x}"
+    print sprintf("% 3d", x)
   end
 end
 puts ""
