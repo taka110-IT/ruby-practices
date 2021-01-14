f23aabf86e085e90b1b0c1e584796f36037f0ecf
taka110 <bigtree.aska@gmail.com>
2021-01-15 01:25:34 +0900

    bowling・rubocop修正後

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -6,11 +6,11 @@ scores = score.chars
 
 shots = []
 scores.each do |s|
-  if s == 'X'
-    shots << 10
-  else
-    shots << s.to_i
-  end
+  shots << if s == 'X'
+             10
+           else
+             s.to_i
+           end
 end
 
 shot_count = 0 # 何投目か
@@ -21,28 +21,19 @@ total_point = 0 # 合計点
 shots.each_with_index do |s, i|
   shot_count += 1
   frame_point += s
-  if frame_count == 10 # 10フレーム目3投目の判定と処理
-    if shot_count == 1 && s == 10 # ストライクの場合
-      total_point += frame_point + shots[i + 1] + shots[i + 2]
-      break
-    elsif frame_point == 10 # スペアの場合
-      total_point += frame_point + shots[i + 1]
-      break
-    end
+  if frame_count == 10 && shot_count == 1 && s == 10 # 10フレーム目3投目の判定とストライクの判定と処理
+    total_point += frame_point + shots[i + 1] + shots[i + 2]
+    break
+  elsif frame_count == 10 && frame_point == 10 # 10フレーム目3投目の判定とスペアの判定と処理
+    total_point += frame_point + shots[i + 1]
+    break
   end
 
   if shot_count == 1 && s == 10 # ストライクの判定と処理
-    total_point += frame_point + shots[i + 1] + shots[i + 2]
-    frame_count += 1
-    frame_point = 0
-    shot_count = 0
-    next
+    total_point += shots[i + 1] + shots[i + 2]
+    shot_count += 1
   elsif frame_point == 10 # スペアの判定と処理
-    total_point += frame_point + shots[i + 1]
-    frame_count += 1
-    frame_point = 0
-    shot_count = 0
-    next
+    total_point +=  shots[i + 1]
   end
 
   next unless shot_count == 2 # 2投目の後に次のフレームへ
